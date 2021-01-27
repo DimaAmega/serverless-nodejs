90deacd9025869d4280800b57086fd687f78bebe
Stanislav <astatsegre@gmail.com>
2021-01-27 16:03:29 +0300

    Add initial NodeJS SDK

---
+++ README.md
@@ -0,0 +1,83 @@
+# Serverless cloud functions
+## Contents
+
+1. [Quick Start](#Quick-Start)
+2. [Examples](#Examples)
+3. [Features](#Features)
+4. [Community](#Community)
+5. [Licensing](#Licensing)
+
+## Quick Start
+
+1. Install:
+    ```
+    npm i @selectel/sels8s
+    ```
+2. Go to [Cloud platform](https://my.selectel.ru/vpc) -> All users. Create new user (or use existing one) and add it to your project.
+3. Go to Cloud platform -> Access. Select user and any region. Click on "Download" button. rc.sh file will be downloaded.
+4. Set environment variables (you can get them from rc.sh file):
+    - OS_USERNAME
+    - OS_PASSWORD
+    - OS_PROJECT_ID
+    - OS_PROJECT_DOMAIN_NAME
+    - OS_USER_DOMAIN_NAME
+5. Create your own cloud function:
+    ```
+   const client = require("sels8s");
+   
+   const functionConfig = {
+     action_name: "my_first_function",
+     function_id: null, // will be added later
+     function_name: "main",
+     file_name: "hello.js",
+     limits: { memory: 256 },
+     module_name: "hello",
+     runtime: "nodejs",
+     version: "12",
+     env_vars: {},
+   };
+   
+   client
+     .uploadModule("./hello.js")
+     .then(({ function_id }) => {
+       return client.startCreateFunction({ ...functionConfig, ...{ function_id } });
+     })
+     .then(() => {
+       client.getFunction(functionConfig.action_name)
+     })
+     .then(({status}) => {
+       if (status.status === "IN PROGRESS") {
+         // retry get function, until you get status "COMPLETE". This means function is ready for invoke.
+       }
+     })
+     .then(() => {
+       return client.startInvokeFunction(functionConfig.action_name, {inParams: "test"})
+     }).then(({activationId}) => {
+     client.getActivationResult(activationId).catch((err) => {
+       // retry getActivationResult if receive 404. It means that invoke hasn't finished yet.
+     })
+     client.getActivationLogs(activationId).catch((err) => {
+       // retry getActivationResult if receive 404. It means that invoke hasn't finished yet.
+     })
+   })
+    ```
+## Examples
+
+You can see examples of using cloud functions
+[here](https://github.com/selectel/serverless_functions_examples_nodejs)
+
+## Features
+
+[Cloud functions](https://selectel.ru/services/cloud/serverless/)
+
+[Docs](https://kb.selectel.ru/docs/selectel-cloud-platform/serverless/description/)
+
+## Community
+
+[GitHub](https://github.com/selectel/serverless-nodejs)
+
+For all questions - sls@selectel.ru
+
+## Licensing
+
+Serverless is licensed under the [MIT License](https://github.com/selectel/serverless-nodejs/LICENSE).
---
+++ index.js
@@ -0,0 +1,181 @@
+const fs = require('fs');
+
+const axios = require("axios");
+const formData = require("form-data");
+
+const AUTH_URL =
+  process.env.OS_AUTH_URL || "https://api.selvpc.ru/identity/v3/auth/tokens";
+const API_URL =
+  process.env.SELECTEL_API_S8S_URL ||
+  "https://ru-1.api.serverless.selcloud.ru/v1";
+const ENV_VARS = {
+  OS_USERNAME: process.env.OS_USERNAME,
+  OS_PASSWORD: process.env.OS_PASSWORD,
+  OS_PROJECT_DOMAIN_NAME: process.env.OS_PROJECT_DOMAIN_NAME,
+  OS_PROJECT_ID: process.env.OS_PROJECT_ID,
+  OS_USER_DOMAIN_NAME: process.env.OS_USER_DOMAIN_NAME,
+};
+
+const client = {};
+const api = axios.create({ baseURL: API_URL });
+const authData = {
+  auth: {
+    identity: {
+      methods: ["password"],
+      password: {
+        user: {
+          name: ENV_VARS.OS_USERNAME,
+          domain: {
+            name: ENV_VARS.OS_USER_DOMAIN_NAME,
+          },
+          password: ENV_VARS.OS_PASSWORD,
+        },
+      },
+    },
+    scope: {
+      project: {
+        id: ENV_VARS.OS_PROJECT_ID,
+        domain: { name: ENV_VARS.OS_PROJECT_DOMAIN_NAME },
+      },
+    },
+  },
+};
+
+let xAuthToken;
+
+api.interceptors.response.use((response) => response.data, (error) => {
+  if (error && error.response && error.response.status === 401) {
+    // delete, because header is not fresh anymore
+    delete error.config.headers['X-Auth-Token'];
+    return auth().then(() => api.request(error.config));
+  }
+  return Promise.reject(error);
+});
+
+api.interceptors.request.use((config) => {
+  if (xAuthToken) {
+    return Promise.resolve(config);
+  }
+  checkEnvVars();
+  return auth().then(() => {
+    // set headers for current request
+    config.headers["X-Auth-Token"] = xAuthToken;
+    config.headers["project-id"] = ENV_VARS.OS_PROJECT_ID;
+    return config;
+  });
+});
+
+const auth = () => {
+  return axios.post(AUTH_URL, authData).then(({ headers }) => {
+    xAuthToken = headers["x-subject-token"];
+    // set headers for future requests
+    api.defaults.headers["X-Auth-Token"] = xAuthToken;
+    api.defaults.headers["project-id"] = ENV_VARS.OS_PROJECT_ID;
+  });
+};
+
+const checkEnvVars = () => {
+  let emptyEnvVars = [];
+  for (let [key, value] of Object.entries(ENV_VARS)) {
+    if (value == null) {
+      emptyEnvVars.push(key);
+    }
+  }
+  if (emptyEnvVars.length) {
+    const errorMessage = `Provide environment variables ${emptyEnvVars.join(", ")}`;
+    console.error(errorMessage)
+    throw new Error(errorMessage);
+  }
+};
+
+client.startCreateFunction = (functionConfig) => {
+  return api.post(`${API_URL}/functions/create`, functionConfig);
+};
+
+client.startEditFunction = (name, functionConfig) => {
+  return api.post(`${API_URL}/functions/${name}/edit`, functionConfig);
+};
+
+client.getFunction = (name) => {
+  return api.get(`${API_URL}/functions/${name}`);
+};
+
+client.getFunctions = (skip, limit, name, sort_field, sort_type = "desc") => {
+  const params = { skip, limit, name, sort_field, sort_type };
+  return api.get(`${API_URL}/functions`, { params });
+};
+
+client.deleteFunction = (name) => {
+  return api.delete(`${API_URL}/functions/${name}`);
+};
+
+client.startInvokeFunction = (name, payload) => {
+  return api.post(`${API_URL}/functions/${name}/invoke`, payload);
+};
+
+client.publishFunction = (name, isPublish) => {
+  return api.post(`${API_URL}/functions/${name}/publish`, {
+    publish: !!isPublish,
+  });
+};
+
+client.uploadModule = (pathToFile) => {
+  const form = new formData();
+  form.append("module", fs.createReadStream(pathToFile));
+  return api.post(`${API_URL}/modules/upload`, form, {
+    headers: form.getHeaders(),
+  })
+};
+
+client.getModule = (id) => {
+  return api.get(`${API_URL}/modules/${id}`);
+};
+
+client.getModules = () => {
+  return api.get(`${API_URL}/modules`);
+};
+
+client.deleteModule = (id) => {
+  return api.delete(`${API_URL}/modules/${id}`);
+};
+
+// since and upto - timestamp
+client.getActivations = (action_name, limit, skip, since, upto) => {
+  const params = {action_name, limit, since, skip, upto};
+  return api.get(`${API_URL}/activations`, { params });
+};
+
+client.getActivationResult = (id) => {
+  return api.get(`${API_URL}/activations/${id}/result`);
+};
+
+client.getActivationLogs = (id) => {
+  return api.get(`${API_URL}/activations/${id}/logs`);
+};
+
+client.getFeedActions = () => {
+  return api.get(`${API_URL}/feed_actions`);
+};
+
+client.getFeeds = () => {
+  return api.get(`${API_URL}/feeds`);
+};
+
+client.getFeed = (name) => {
+  return api.get(`${API_URL}/feeds/${name}`);
+};
+
+client.createFeed = (feed_name, feed_action, action_name, parameters) => {
+  return api.put(`${API_URL}/feeds/create`, {
+    feed_name,
+    feed_action,
+    action_name,
+    parameters,
+  });
+};
+
+client.deleteFeed = (name) => {
+  return api.delete(`${API_URL}/feeds/${name}`);
+};
+
+module.exports = client;
